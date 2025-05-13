466eec47abdaaf6234244ffaaa269ce425d92fab
LEO XU <LEOXC1571@163.com>
2025-05-13 16:02:24 +0800

    updated

--- google_scholar_crawler/main.py
+++ google_scholar_crawler/main.py
@@ -27,29 +27,14 @@ import jsonpickle
 import json
 from datetime import datetime
 import os
-import time
 
-max_attempts = 100
-wait_seconds = 600  # 10 minutes
+# Setup proxy
+pg = ProxyGenerator()
+pg.FreeProxies()  # Use free rotating proxies
+scholarly.use_proxy(pg)
 
-for attempt in range(1, max_attempts + 1):
-    try:
-        print(f"Attempt {attempt}:")
-        # Setup proxy
-        pg = ProxyGenerator()
-        pg.FreeProxies()  # Use free rotating proxies
-        scholarly.use_proxy(pg)
-        
-        author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
-        scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
-        print(f"Attempt {attempt} success")
-        break  # Exit loop on first success
-    except Exception as e:
-        print(f"Attempt {attempt} failed with error: {e}")
-        time.sleep(wait_seconds)
-else:
-    print("All 100 attempts failed.")
-
+author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
 name = author['name']
 author['updated'] = str(datetime.now())
 author['publications'] = {v['author_pub_id']:v for v in author['publications']}
@@ -57,7 +42,6 @@ print(json.dumps(author, indent=2))
 os.makedirs('results', exist_ok=True)
 with open(f'results/gs_data.json', 'w') as outfile:
     json.dump(author, outfile, ensure_ascii=False)
-
 shieldio_data = {
   "schemaVersion": 1,
   "label": "citations",
@@ -65,3 +49,48 @@ shieldio_data = {
 }
 with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
     json.dump(shieldio_data, outfile, ensure_ascii=False)
+
+
+# from scholarly import scholarly, ProxyGenerator
+# import jsonpickle
+# import json
+# from datetime import datetime
+# import os
+# import time
+
+# max_attempts = 100
+# wait_seconds = 600  # 10 minutes
+
+# for attempt in range(1, max_attempts + 1):
+#     try:
+#         print(f"Attempt {attempt}:")
+#         # Setup proxy
+#         pg = ProxyGenerator()
+#         pg.FreeProxies()  # Use free rotating proxies
+#         scholarly.use_proxy(pg)
+        
+#         author: dict = scholarly.search_author_id(os.environ['GOOGLE_SCHOLAR_ID'])
+#         scholarly.fill(author, sections=['basics', 'indices', 'counts', 'publications'])
+#         print(f"Attempt {attempt} success")
+#         break  # Exit loop on first success
+#     except Exception as e:
+#         print(f"Attempt {attempt} failed with error: {e}")
+#         time.sleep(wait_seconds)
+# else:
+#     print("All 100 attempts failed.")
+
+# name = author['name']
+# author['updated'] = str(datetime.now())
+# author['publications'] = {v['author_pub_id']:v for v in author['publications']}
+# print(json.dumps(author, indent=2))
+# os.makedirs('results', exist_ok=True)
+# with open(f'results/gs_data.json', 'w') as outfile:
+#     json.dump(author, outfile, ensure_ascii=False)
+
+# shieldio_data = {
+#   "schemaVersion": 1,
+#   "label": "citations",
+#   "message": f"{author['citedby']}",
+# }
+# with open(f'results/gs_data_shieldsio.json', 'w') as outfile:
+#     json.dump(shieldio_data, outfile, ensure_ascii=False)
